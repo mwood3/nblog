baf842c72f30de958058af92923ec7c157552f8a
mwood3 <marshall@complymd.com>
2010-11-16 22:42:31 -0600

    first commit (rails seketon)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nblog::Application.config.secret_token = '65f8a2eacdcc980a901e83d28b2bf86c8bce227c278f8d13f9570824a27d7547d6650d5b81780f7d08597bd72bbed5f9022421899a681f04661dfc0ddf8ce5a1'
